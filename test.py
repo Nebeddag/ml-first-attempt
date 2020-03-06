7877e82171d54e5e5a6c56b8fb914a4fc1b4cab0
Иван <nebeddag@live.com>
2020-03-06 23:37:12 +0300

    pythonPath added to config. gitignore added. test updated.

---
+++ test.py
@@ -0,0 +1,16 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#%%
+p = np.linspace(0,20,100)
+plt.plot(p,np.sin(15*p))
+
+#%%
+plt.show()
+
+# %%
+print(p)
+
+# %%
