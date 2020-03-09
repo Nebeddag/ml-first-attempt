1ef09981906410ec55d1956172b01fe3e890c8e7
Иван <nebeddag@live.com>
2020-03-09 19:42:06 +0300

    loading data from .bpm files added

--- test.py
+++ test.py
@@ -2,7 +2,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import helpers.dataloader as dloader
 
+'''
 #%%
 p = np.linspace(0,20,100)
 plt.plot(p,np.sin(15*p))
@@ -12,5 +14,7 @@ plt.show()
 
 # %%
 print(p)
+'''
 
-# %%
+#%%
+tmp = dloader.readimgs('data\\photos1\\0\\')
