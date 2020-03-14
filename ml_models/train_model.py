ec0f6a53a7757c62914ffb1874796e6955981c1e
Иван <nebeddag@live.com>
2020-03-14 21:20:12 +0300

    cosmetic changes

--- ml_models/train_model.py
+++ ml_models/train_model.py
@@ -2,15 +2,17 @@ from torch import nn, optim, from_numpy
 from ml_models.logistic_regression import LogisticRegression
 from ml_models.simple_nn import SimpleNN
 
+
 def train_model(x_data, y_data):
     epoch_count = 10000
+    learning_rate = 0.02
 
     x_data = x_data.float()
     y_data = y_data.float()
     model = SimpleNN(80*60*3, 20, 6, 1)
     #model = LogisticRegression(80*60*3, 1)
-    criterion = nn.BCELoss(reduction='mean')
-    optimizer = optim.SGD(model.parameters(), lr=0.01)
+    criterion = nn.BCELoss(reduction = 'mean')
+    optimizer = optim.SGD(model.parameters(), lr = learning_rate)
 
     for epoch in range(epoch_count):
         # Forward pass: Compute predicted y by passing x to the model
